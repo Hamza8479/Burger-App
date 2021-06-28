1429ca10e6173559bb786e77c390a28becc1690d
Hamza <hamzazaman500@gmail.com>
2021-06-28 11:15:00 +0500

    Starting Implementation of The APP

---
+++ burger-builder/src/components/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Auxiliary from "../../hoc/Auxiliary";
+
+const BurgerBuilder = () => {
+  return (
+    <Auxiliary>
+      <div>Burger</div>
+      <div>Build Controls</div>
+    </Auxiliary>
+  );
+};
+
+export default BurgerBuilder;
