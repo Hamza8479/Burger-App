e252060b5ef7db504b7ea2204d46ce408513926e
Hamza <hamzazaman500@gmail.com>
2021-07-27 16:04:40 +0500

    Display and Update Burger Price

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,11 +36,38 @@ const BurgerBuilder = () => {
     setSate({ totalPrice: newPrice, ingredients: updatedIngredients });
   };
 
-  const removeIngredientHandler = (type) => {};
+  const removeIngredientHandler = (type) => {
+    const oldCount = state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENTS_PRICES[type];
+    const oldPrice = state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+
+    setSate({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  const disabledInfo = {
+    ...state.ingredients,
+  };
+  for (let key in disabledInfo) {
+    disabledInfo[key] = disabledInfo[key] <= 0;
+  }
   return (
     <Auxiliary>
       <Burger ingredients={state.ingredients} />
-      <BuildControls ingredientAdded={addIngredientHandler} />
+      <BuildControls
+        ingredientAdded={addIngredientHandler}
+        ingredientRemoved={removeIngredientHandler}
+        disabled={disabledInfo}
+        price={state.totalPrice}
+      />
     </Auxiliary>
   );
 };
