a032b19fc54443fbfb51f975ddadf74cb87a2656
Hamza <hamzazaman500@gmail.com>
2021-08-21 17:28:04 +0500

    Burger App

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Auxiliary from "../../hoc/Auxiliary";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = (props) => {
+  const [state, setState] = useState({
+    showSideDrawer: false,
+  });
+  const sideDrawerCloseHandler = () => {
+    setState({ showSideDrawer: false });
+  };
+
+  const menuHandler = (prev) => {
+    setState({ showSideDrawer: !prev.showSideDrawer });
+  };
+
   return (
     <Auxiliary>
-      <Toolbar />
-      <SideDrawer />
+      <Toolbar menu={menuHandler} />
+      <SideDrawer closed={sideDrawerCloseHandler} open={state.showSideDrawer} />
       <main className={classes.Content}>{props.children}</main>
     </Auxiliary>
   );
--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,15 @@ import React from "react";
 import classes from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggler from "../SideDrawer/DrawerToggle/DrawerToggler";
 const Toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
-      <div>MENU</div>
+      <DrawerToggler menu={props.menu} />
       <div className={classes.Logo}>
         <Logo />
       </div>
-      <nav>
+      <nav className={classes.DesktopOnly}>
         <NavigationItems />
       </nav>
     </header>
